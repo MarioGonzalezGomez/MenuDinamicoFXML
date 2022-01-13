634e66ee3becfd5014d6b1f0e269dfcd5e660a35
MarioGonzalezGomez <80425131+MarioGonzalezGomez@users.noreply.github.com>
2022-01-13 11:57:53 +0100

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.interfazdinamicafxml {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.interfazdinamicafxml to javafx.fxml;
-    exports com.example.interfazdinamicafxml;
-}
