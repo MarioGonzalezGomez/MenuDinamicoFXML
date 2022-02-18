8e8838fc7b898f14415538f2db98bf5aa3c716da
MarioGonzalezGomez <ggperpi@gmail.com>
2022-02-18 09:46:26 +0100

    RetofitOk

--- MenuDinamico/src/main/java/mggcode/menudinamico/Controlador.java
+++ MenuDinamico/src/main/java/mggcode/menudinamico/Controlador.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 import javafx.scene.control.SelectionMode;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
@@ -25,11 +26,13 @@ import java.util.List;
 import java.util.ResourceBundle;
 
 public class Controlador implements Initializable {
-    AccesoDatosRest restService = APIRestConfig.getService();
+    private AccesoDatosRest restService = APIRestConfig.getService();
+    private ObservableList<Persona> personas;
+    private DetalleController detalleController;
+    private GraficoController graficoController;
 
     @FXML
     private ListView<Persona> lista;
-    private ObservableList<Persona> personas;
 
     @FXML
     private HBox listaDetalle;
@@ -61,14 +64,22 @@ public class Controlador implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        detalleController = new DetalleController();
+        graficoController = new GraficoController();
         listaDetalle.setVisible(false);
         listaDetalle.setTranslateX(-100);
         personas = cargarPersonas();
-        if (personas == null) {
-            System.out.println("Por qué está vacia??");
-        }
+
+        personas.stream().forEach(x -> System.out.println(x.getName()));
+
         lista.setItems(personas);
         lista.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+        lista.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                detalleController.mostrarDetalle(new Image("https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"), newValue.getName(), newValue.getUsername(),
+                        newValue.getPhone(), newValue.getEmail());
+            }
+        });
     }
 
     private ObservableList<Persona> cargarPersonas() {
@@ -117,6 +128,10 @@ public class Controlador implements Initializable {
                 // lbl4.setVisible(false);
             }
             if (event.getSource().equals(imageContactos)) {
+                // detalleController.ocultarDetalle();
+                listaDetalle.setVisible(true);
+                listaDetalle.setTranslateX(125);
+
                 //  lbl1.setVisible(false);
                 //  lbl2.setVisible(false);
                 //  lbl3.setVisible(true);
